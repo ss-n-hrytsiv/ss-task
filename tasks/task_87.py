b28e482e72880cefe9d395806cbb270891fc1f40
ss-o-furda <mobillord83@gmail.com>
2019-10-16 13:52:46 +0300

    Refactor Orik's tasks

--- tasks/task_87.py
+++ tasks/task_87.py
@@ -4,33 +4,6 @@
 
 def task_87(n, m):
     """Дано натуральні n, m. Отримати суму m останніх цифр числа n."""
-    # print('Введіть натуральні N i M, щоб отримати суму m останніх цифр числа n.')
-    # while True:
-    #     n = input('Введіть натуральне N:\n')
-    #     if not n.isdigit():
-    #         print('Схоже Ви ввели не число...')
-    #         continue
-    #     elif n == 0:
-    #         print('0 не є натуральним числом!')
-    #         continue
-    #     else:
-    #         n = list(map(int, n))choice_task_dict[choice_task]()
-    #         while True:
-    #             m = input('Введіть натуральне M:\n')
-    #             if not m.isdigit():
-    #                 print('Схоже Ви ввели не число...')
-    #                 continue
-    #             elif m == 0:
-    #                 print('0 не є натуральним числом!')
-    #                 continue
-    #             elif int(m) > len(n):
-    #                 print(
-    #                     f'Вибачте, але у максимальне значення для M може бути {len(n)}')
-    #                 continue
-    #             else:
-    #                 m = int(m)
-    #                 break
-    #         break
     result = sum(n[-m:])
 
     return result
