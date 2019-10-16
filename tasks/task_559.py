5813692b0869779478f7c1a99ebcf08b16966f29
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2019-10-16 16:01:43 +0300

    asdas

--- tasks/task_559.py
+++ tasks/task_559.py
@@ -5,12 +5,9 @@ __author__ = 'Orest Furda'
 
 def task_559(n):
     """Дано натуральне n. Знайти всі, менші n числа Мерсенна."""
-    # n = input("Enter n: ")
+
     num_mers = [(2 ** i - 1) for i in range(1, n + 1)]
     simple_num = []
-
-    # for i in range(1, n+1):
-    #     num_mers.append(2 ** i - 1)
 
     for count in range(2, n+1):
         i = 2
@@ -22,9 +19,6 @@ def task_559(n):
         else:
             simple_num.append(count)
     simple_num_mers = [(2 ** i + 1) for i in simple_num]
-    # for i in simple_num:
-    #     simple_num_mers.append(2 ** i - 1)
-
     return num_mers, simple_num_mers
 
 
--- tasks/task_num_107.py
+++ tasks/task_num_107.py
@@ -1,11 +1,43 @@
+<<<<<<< HEAD
 __author__ = 'Maks Ivanov'
 
 def task_num_107():
     """ Дано целое число m > 1 Получить наибольшое целое к, при котором 4^к < m """
     m = int(input('\'Task 107\' Input your natural number : '))
+=======
+""" We have a natural number m > 1. Get the greatest integer k, at which 4^k < m
+"""
+
+
+def task_num_107(abra):
+    """ Дано целое число m > 1 Получить наибольшое целое к, при котором 4^к < m
+        (We have a natural number m > 1. Get the greatest integer k, at which 4^k < m)
+    """
+>>>>>>> 3468b88d7dfd3d50d7f9453c5e67e1fc71210f9e
     counter = 1
     a = 4
-    while a < m:
+    while a < abra:
         a *= a
         counter += 1
     return counter
+
+
+def task107_menu():
+    while True:
+        print('-' * 100)
+        print('Function logic: ' + '\n' + task_num_107.__doc__)
+        print('-' * 50)
+        print('Input only number')
+        print('If you wnat to exit, just input smth from: q /quit / exit /stop /terminate')
+        print('-' * 50)
+
+        user_number = input('\'Task 107\' Input your natural number : ')
+        try:
+            m = int(user_number)
+            print(task_num_107(m))
+        except ValueError:
+            if user_number.lower() in ['q', 'quit', 'exit', 'stop', 'terminate']:
+                break
+
+            print('n - must be an int type!')
+            continue
