4a2262271c4786f3e8037c29c997f0685b6d4b8a
ss-o-furda <mobillord83@gmail.com>
2019-10-29 13:29:02 +0200

    fix test for nazar

--- tests/test_for_nazar_hrytsiv.py
+++ tests/test_for_nazar_hrytsiv.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 sys.path.append('../')
+from unittest.mock import patch
 from tasks.task_86_a_b import task_86_a
 from tasks.task_86_a_b import task_86_b
 from tasks.task_330 import task330
@@ -9,21 +10,21 @@ from tasks.task_330 import task330
 class Test(unittest.TestCase):
     def test_task86_a(self):
             expected_answer = 2
-            expected_input = str(10)
+            expected_input = 10
             actual_answer = task_86_a(expected_input)
             self.assertEqual(actual_answer, expected_answer)
 
     def test_task86_b(self):
         expected_answer = 15
-        expected_input = str(12345)
+        expected_input = 12345
         actual_answer = task_86_b(expected_input)
         self.assertEqual(actual_answer, expected_answer)
 
     def test_task330(self):
         expected_answer = [0, 6]
-        expected_input = str(10)
-        actual_answer = task330(expected_input)
-        self.assertEqual(actual_answer, expected_answer)
+        expected_input = 10
+        with patch('builtins.input', return_value=expected_input) as _raw_input:
+            self.assertEqual(task330(), expected_answer)
 
 
 if __name__ == '__main__':
